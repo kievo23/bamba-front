615bf629fe601b47df622948b29eeda020cd70d3
kev <kelvinchege@gmail.com>
2024-01-07 17:46:36 +0300

    change background to white

--- src/components/ModalConfirm.vue
+++ src/components/ModalConfirm.vue
@@ -5,7 +5,8 @@
             <font-awesome-icon icon="fa-solid fa-question-circle " class="fa-2x"/>
         </div>
         <div class="body">
-            <p>Buy credit worth {{ amount }} for {{ phone }}</p>
+            <p>Buy credit worth <span class="quote"> {{ amount }} </span> for </p>
+            <p><span class="quote"> {{ phone }} </span></p>
             <button class="airtime-form-btn">
                  Pay Via {{phone}}
             </button>
